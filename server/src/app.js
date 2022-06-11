b0632a0e210948954e41c53176b854de85ea3f9e
zainab <zeinab@kuwaitnet.dev>
2022-06-11 19:43:42 +0300

    add CORS middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const planetsRouter = require('./routes/planets/planets.router');
-
+// cors on top
+app.use(cors({
+    origin:'http://localhost:3000',
+}))
 app.use(express.json());
 app.use(planetsRouter)
 
