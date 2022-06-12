de9e3912fada4b551653cc5c1858b3b4e7eaf01d
zainab <zeinab@kuwaitnet.dev>
2022-06-12 17:09:04 +0300

    WIP add planets model

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,2 +1,33 @@
-const planets = [];
-module.exports = planets;
+const parse = require('csv-parse');  // csv-parse library
+const fs = require('fs'); // built in file system
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet){
+    return planet['koi_disposition'] === 'CONFIRMED'
+    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11 && planet['koi_prad'] < 1.6;
+}
+
+//module 6, vedio 6
+// read our file
+fs.createReadStream('kepler_data.csv') //give us events emitter , react with on
+    .pipe(parse({
+        Comment : '#',  // line start with # is comment
+        columns : true,   
+    }))  // will return nicely parsed js object with key, value
+
+    .on('data', (data)=>{
+        if (isHabitablePlanet(data)){
+            habitablePlanets.push(data);
+        }  
+    })
+    .on('error', (err)=>{
+        console.log(err);
+    })
+    .on('end',()=>{  // when end we won't be receving data
+        // console.log(`${data.length} planets found`);
+        console.log("bye!!");
+    });
+
+    module.exports = {
+       planets: habitablePlanets,
+    }
