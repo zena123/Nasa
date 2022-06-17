62517038756ad7bdd64ef2dc9efeb5dc470318a6
zainab <zeinab@kuwaitnet.dev>
2022-06-17 15:35:45 +0300

    loading data on startup

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,5 +1,7 @@
-const parse = require('csv-parse');  // csv-parse library
+const path = require('path');
 const fs = require('fs'); // built in file system
+const { parse } = require('csv-parse');  // csv-parse library
+
 const habitablePlanets = [];
 
 function isHabitablePlanet(planet){
@@ -7,27 +9,52 @@ function isHabitablePlanet(planet){
     && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11 && planet['koi_prad'] < 1.6;
 }
 
-//module 6, vedio 6
-// read our file
-fs.createReadStream('kepler_data.csv') //give us events emitter , react with on
-    .pipe(parse({
-        Comment : '#',  // line start with # is comment
-        columns : true,   
-    }))  // will return nicely parsed js object with key, value
+/*
+PECAP PROMISE
+const promise = new Promise((resolve,reject ) =>{
+    resolve(42)
+});
+    hen:eccepts callback function with whatever result is
+promise.then((result)=>{
 
-    .on('data', (data)=>{
-        if (isHabitablePlanet(data)){
-            habitablePlanets.push(data);
-        }  
+})
+
+const result =  await promise;
+
+*/
+
+
+function LoadPlanetsData(){
+    return new Promise((resolve, reject)=>{
+        fs.createReadStream(path.join(__dirname,'..', '..','data','kepler_data.csv'))//give us events emitter , react with on
+          .pipe(parse({
+                comment: '#',  // line start with # is comment
+                columns: true,   
+            }))  // will return nicely parsed js object with key, value
+
+          .on('data', (data)=>{
+                if (isHabitablePlanet(data)){
+                    habitablePlanets.push(data);
+                }  
+            })
+         .on('error', (err)=>{
+            console.log(err);
+            reject(err);
+        })
+         .on('end',()=>{  // when end we won't be receving data
+            console.log(`${habitablePlanets.length} planets found`);
+            console.log("bye!!");
+
+            //call our resolve function when  finish parsing data
+            resolve();
+        });
+
     })
-    .on('error', (err)=>{
-        console.log(err);
-    })
-    .on('end',()=>{  // when end we won't be receving data
-        // console.log(`${data.length} planets found`);
-        console.log("bye!!");
-    });
+    
+
+}
 
     module.exports = {
+        LoadPlanetsData,
        planets: habitablePlanets,
     }
--- server/src/server.js
+++ server/src/server.js
@@ -1,17 +1,30 @@
 const http =  require('http');
 const app = require('./app');  // relative import
+const {LoadPlanetsData} = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;   // react port is 3000 
 
 
 
-//bulid node http server depande on built in http server
+//build node http server depanding on built in http server
 
 //express like a middle ware added on top of http server
 
 const server = http.createServer(app);
-server.listen(PORT, ()=>{
-    console.log(`listening to port ${PORT}....`)
-})
+
+// await can be called only for async so we craete a function
+async function startServer(){
+    // we will await the function so data is available for requests coming & here we don't need return value , we already set our data
+    await LoadPlanetsData();
+
+    server.listen(PORT, ()=>{
+        console.log(`listening to port ${PORT}....`)
+    })
+}
+startServer();
+
+
+
+
 
 
